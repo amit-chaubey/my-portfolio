b4afc8386cff6d3bf495c790b1c19a2a5283b830
AmitChaubey <amit.katyayana@gmail.com>
2025-02-23 16:00:36 +0000

    Fix CSS loading in production build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
